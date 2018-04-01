17d7abd5f197c5d678e4a9f31dd99d3c1f7932a6
jenny-l-li <lijenny2772@gmail.com>
2018-04-01 00:48:50 -0700

    fixed things

--- script2.js
+++ script2.js
@@ -4,7 +4,7 @@ var comment3 = "";
 
 function calcScore1() { //at least n items
     var x = Math.floor((Math.random() * 10) + 1);
-    comment1 = "very full... lots of stuff... yes";
+    comment1 = "\"very full... lots of stuff... yes\"";
     return x;
 
     // if (size <= 3)
@@ -17,14 +17,14 @@ function calcScore1() { //at least n items
 
 function calcScore2() { //how much frosting? idk
     var x = Math.floor((Math.random() * 10) + 1);
-    comment2 = "great sense of aesthetic. a+";  
+    comment2 = "\"great sense of aesthetic. a+\"";  
     return x;
 }
 
 function calcScore3() { //very specific items
     //param: item1, item2, item3, item4, item5
     var x = Math.floor((Math.random() * 5) + 1); //1-5 inclusive
-    comment3 = "i have very specific tastes that you haven't satisfied"; 
+    comment3 = "\"i have very specific tastes that you haven't satisfied\""; 
     return x;
 }
 
@@ -32,6 +32,9 @@ function showScores(i){
     setTimeout(function(){ 
         $(`#j${i}`).removeClass('hide'); 
         $(`#j${i}`).addClass('show'); 
+        // $(`#w${i}`).css("transform","translate(0%, 0%)")
+        $(`#w${i}`).css("height", "0");
+
     }, 4000);
 }
 
@@ -45,34 +48,37 @@ function includes(item, array){
 
 function showComments(n){
     console.log(n);
+    var target = $('.text-container h1');
     switch (n) {
             case 1:
-                $('.text-container h1').text(`${comment1}`);
-                console.log("called1")
+                target.text(`${comment1}`);
+                target.css("color","#9193df");
                 break;
             case 2:
-                $('.text-container h1').text(`${comment2}`);
-                console.log("called2")
+                target.text(`${comment2}`);
+                target.css("color","#D271BC");
                 break;
             case 3:
-                $('.text-container h1').text(`${comment3}`);
-                console.log("called")
+                target.text(`${comment3}`);
+                target.css("color","#ed92d8");
                 break;
         }
 }
 
 function doHover(i){
-    if ($(`#j${i}`).hasClass('show')){
+
         $(`#${i}`).hover(function(){
+            if ($(`#j${i}`).hasClass('show')){
             $('.text-container p').css("opacity", "0");
             $('.text-container h1').css("font-size", "2em");
             $(this).css("opacity", ".6");
             // console.log($(`#${i}`).attr('id'));
             showComments(i);
+        }
         }, function(){
             $(this).css("opacity", "1");
         });
-    }
+
 }
 
 $('document').ready(function() { 
@@ -95,15 +101,15 @@ $('document').ready(function() {
         $(`#j${i}`).text(`${score}`);
         showScores(i); //the array will be a parameter
 
-        doHover(i);
+        
     }
 
     var audio = new Audio("drum.mp3");
     audio.play();
 
-    // for (var i =1; i<=3; i++)
-    // {
-
-    // }
+    for (var i =1; i<=3; i++)
+    {
+        doHover(i);
+    }
     
 });
